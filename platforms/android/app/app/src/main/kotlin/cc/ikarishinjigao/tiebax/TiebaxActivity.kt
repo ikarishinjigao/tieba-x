949374ec417d1403ed0f87927e88547b4fc3381f
Robbin Huang <ikarishinjigao@gmail.com>
2024-09-21 02:40:31 +0900

    feat: add edge to edge for android

--- platforms/android/app/app/src/main/kotlin/cc/ikarishinjigao/tiebax/TiebaxActivity.kt
+++ platforms/android/app/app/src/main/kotlin/cc/ikarishinjigao/tiebax/TiebaxActivity.kt
@@ -1,8 +1,12 @@
 package cc.ikarishinjigao.tiebax
 
+import android.graphics.Color.TRANSPARENT
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.DisposableEffect
 import cc.ikarishinjigao.tiebax.core.designsystem.theme.TiebaxTheme
 import cc.ikarishinjigao.tiebax.ui.TiebaxApp
 import org.koin.compose.KoinContext
@@ -12,7 +16,21 @@ class TiebaxActivity : ComponentActivity() {
     savedInstanceState: Bundle?,
   ) {
     super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
     setContent {
+      DisposableEffect(true) {
+        enableEdgeToEdge(
+          statusBarStyle = SystemBarStyle.auto(
+            TRANSPARENT,
+            TRANSPARENT,
+          ),
+          navigationBarStyle = SystemBarStyle.auto(
+            lightScrim,
+            darkScrim,
+          ),
+        )
+        onDispose {}
+      }
       KoinContext {
         TiebaxTheme {
           TiebaxApp()
@@ -21,3 +39,6 @@ class TiebaxActivity : ComponentActivity() {
     }
   }
 }
+
+private val lightScrim = android.graphics.Color.argb(0xe6, 0xFF, 0xFF, 0xFF)
+private val darkScrim = android.graphics.Color.argb(0x80, 0x1b, 0x1b, 0x1b)
--- platforms/android/app/app/src/main/kotlin/cc/ikarishinjigao/tiebax/ui/TiebaxApp.kt
+++ platforms/android/app/app/src/main/kotlin/cc/ikarishinjigao/tiebax/ui/TiebaxApp.kt
@@ -2,18 +2,65 @@ package cc.ikarishinjigao.tiebax.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TiebaxApp(
   viewModel: TiebaAppViewModel = koinViewModel(),
+) {
+  var selectedItem by remember { mutableIntStateOf(0) }
+  val items = listOf("Label", "Label", "Label", "Label")
+  Scaffold(
+    topBar = {
+      TopAppBar(title = { Text("Title") })
+    },
+    bottomBar = {
+      NavigationBar {
+        items.forEachIndexed { index, item ->
+          NavigationBarItem(
+            icon = { Icon(Icons.Default.Face, contentDescription = item) },
+            label = { Text(item) },
+            selected = selectedItem == index,
+            onClick = { selectedItem = index },
+          )
+        }
+      }
+    },
+  ) { innerPadding ->
+    Row(
+      modifier = Modifier
+        .padding(innerPadding),
+    ) {
+      Test(viewModel)
+    }
+  }
+}
+
+@Composable
+fun Test(
+  viewModel: TiebaAppViewModel,
 ) {
   Column(
     modifier = Modifier
